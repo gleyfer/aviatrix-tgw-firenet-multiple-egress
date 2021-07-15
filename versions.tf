5ef8fe6bb45fcd6f30b5c93eaea9bd82ef201c99
Gene Leyfer <gleyfer@aviatrix.com>
2021-07-15 12:33:57 -0400

    Fix Provider version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "2.19.4"
+      version = "2.19.5"
     }
     aws = {
       source = "hashicorp/aws"
